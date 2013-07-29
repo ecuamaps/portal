df098a9d3ca001138d5299ee7f236437914959e7
CesarReyes <cesar.cesarreyes@gmail.com>
2013-07-29 15:55:24 -0500

    Developed the tags product and fixed the activation/deactivation product feature from biz panel

---
+++ application/language/english/products/tags_lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$lang['tags.title'] = 'Add tags and improve your results in the searches';
+$lang['tags.save'] = 'Save';
+$lang['tags.disclaimer'] = 'It Applies for searches but do not appear at the results, average of 20 characters by tag and up to %d words.';
+$lang['tags.success'] = 'Your tags has been saved successfully.';	
+$lang['tags.usedwords'] = 'Used tags';	
+$lang['tags.remainingchars'] = 'Remaining characters';	
